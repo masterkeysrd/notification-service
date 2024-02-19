85bf048a2d5326050112b9675e01cdc724d75512
David Morales <morales.david1997@gmail.com>
2024-02-19 15:37:57 -0400

    refactor: remove comments from SendNotificationRequest class

--- src/main/java/com/masterkeys/notificationservice/service/dto/SendNotificationRequest.java
+++ src/main/java/com/masterkeys/notificationservice/service/dto/SendNotificationRequest.java
@@ -1,42 +1,5 @@
 package com.masterkeys.notificationservice.service.dto;
 
-// public class SendNotificationRequest {
-//     String message;
-//     String category;
-
-//     public SendNotificationRequest() {
-//     }
-
-//     public SendNotificationRequest(String message, String category) {
-//         this.message = message;
-//         this.category = category;
-//     }
-
-//     public String getMessage() {
-//         return message;
-//     }
-
-//     public String getCategory() {
-//         return category;
-//     }
-
-//     public void setMessage(String message) {
-//         this.message = message;
-//     }
-
-//     public void setCategory(String category) {
-//         this.category = category;
-//     }
-
-//     @Override
-//     public String toString() {
-//         return "SendNotificationRequest{" +
-//                 "message='" + message + '\'' +
-//                 ", category='" + category + '\'' +
-//                 '}';
-//     }
-// }
-
 public record SendNotificationRequest(String message, String category) {
     public SendNotificationRequest {
         if (message == null) {
