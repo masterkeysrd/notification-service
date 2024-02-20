97f26944d2de7c058e2f47f2405d4add0ed6812e
David Morales <morales.david1997@gmail.com>
2024-02-19 22:58:21 -0400

    fix(notification): add @Service annotation to NotificationRecorderServiceImpl

--- src/main/java/com/masterkeys/notificationservice/service/impl/NotificationRecorderServiceImpl.java
+++ src/main/java/com/masterkeys/notificationservice/service/impl/NotificationRecorderServiceImpl.java
@@ -5,10 +5,12 @@ import java.util.concurrent.CompletableFuture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
 
 import com.masterkeys.notificationservice.service.NotificationRecorderService;
 import com.masterkeys.notificationservice.service.dto.RecordNotificationRequest;
 
+@Service
 public class NotificationRecorderServiceImpl implements NotificationRecorderService {
 
     final private Logger logger = LoggerFactory.getLogger(NotificationRecorderServiceImpl.class);
