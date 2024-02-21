acbb2e53f3b3c27f573435c16c7e8d30d091df66
David Morales <morales.david1997@gmail.com>
2024-02-21 10:31:15 -0400

    fix(webapp): update navigation links in RootRoute component

--- src/main/webapp/app/routes/RootRoute.tsx
+++ src/main/webapp/app/routes/RootRoute.tsx
@@ -4,6 +4,7 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { Container } from "@mui/system";
 import { Outlet } from "react-router";
+import { Link } from "react-router-dom";
 
 function RootRoute() {
   return (
@@ -13,8 +14,8 @@ function RootRoute() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Notification Service
           </Typography>
-          <Button color="inherit" href="/send-message">Send Message</Button>
-          <Button color="inherit" href="/history">History</Button>
+          <Button color="inherit" component={Link} to="/send-message">Send Message</Button>
+          <Button color="inherit" component={Link} to="/history">History</Button>
         </Toolbar>
       </AppBar>
 
